59777347d351d92a3cb7dcc216260884b8c9a393
Arya Shah <arya.shah82@nmims.edu.in>
2023-11-20 08:15:28 +0530

    Update scrape_news_api_UX.py Refresh MD

--- scripts/scrape_news_api_UX.py
+++ scripts/scrape_news_api_UX.py
@@ -9,25 +9,27 @@ def fetch_news(api_key, keyword):
     data = response.json()
     return data['articles']
 
-def generate_markdown(articles, keyword):
-    with open('README.md', 'a') as file:
-        file.write(f'## News for {keyword}\n\n')
-        file.write('| Title | Source | Published |\n')
-        file.write('| ----- | ------ | --------- |\n')
-        for article in articles:
-            title = article['title']
-            source = article['source']['name']
-            published_at = article['publishedAt'].split('T')[0]  # Format the date
-            url = article['url']
-            file.write(f"| [{title}]({url}) | {source} | {published_at} |\n")
-        file.write('\n')
+def generate_markdown(articles, keyword, file):
+    file.write(f'## News for {keyword}\n\n')
+    file.write('| Title | Source | Published |\n')
+    file.write('| ----- | ------ | --------- |\n')
+    for article in articles:
+        title = article['title']
+        source = article['source']['name']
+        published_at = article['publishedAt'].split('T')[0]  # Format the date
+        url = article['url']
+        file.write(f"| [{title}]({url}) | {source} | {published_at} |\n")
+    file.write('\n')
 
 api_key = os.environ.get('NEWS_API_KEY')  # Get API key from environment variable
 if not api_key:
     raise Exception("No API key found. Set the NEWS_API_KEY environment variable.")
 
-keywords = ["Pharmaceutical Industry", "Life Sciences", "Medical Devices", "Healthcare Sector", "Pharma Market Trends", "FDA Regulations"]  # Example keywords
+# Open the README.md file in write mode to refresh its content
+with open('README.md', 'w') as file:
+    file.write('# Latest News\n\n')
+    keywords = ["Pharmaceutical Industry", "Life Sciences", "Medical Devices", "Healthcare Sector", "Pharma Market Trends", "FDA Regulations"]  # Example keywords
 
-for keyword in keywords:
-    articles = fetch_news(api_key, keyword)
-    generate_markdown(articles, keyword)
+    for keyword in keywords:
+        articles = fetch_news(api_key, keyword)
+        generate_markdown(articles, keyword, file)
